7e2ba841ff894e3b1d314c660ce7568ff7a8c759
LeeJinnccucs <talesofmyworld@gmail.com>
2016-05-26 11:01:27 +0800

    Initial Commit

---
+++ MrtStation1Tests/MrtStation1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MrtStation1Tests.swift
+//  MrtStation1Tests
+//
+//  Created by Jin Lee on 2016/5/26.
+//  Copyright © 2016年 Jin Lee. All rights reserved.
+//
+
+import XCTest
+@testable import MrtStation1
+
+class MrtStation1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
